e6e1ef0ef97bff4673babcbaf95669c98eba8083
54ac <me@54ac.ovh>
2024-12-19 18:53:05 +0100

    remove options link for compatibility

--- src/content-script.ts
+++ src/content-script.ts
@@ -41,28 +41,6 @@ const addStyles = async () => {
 };
 addStyles();
 
-const newOptionsButton = document.createElement("button");
-newOptionsButton.innerText = chrome.i18n.getMessage("optionsButtonText");
-newOptionsButton.onclick = () =>
-	chrome.runtime.sendMessage({ action: "openOptions" });
-newOptionsButton.classList.add("ytp-button", "ytp-panel-options");
-
-const ytOptionsEl = document.getElementsByClassName("ytp-settings-menu")[0];
-const ytOptionsObserver = new MutationObserver(() => {
-	if (
-		!ytOptionsEl ||
-		!ytOptionsEl.querySelector(".ytp-subtitles-options-menu-item") ||
-		ytOptionsEl.querySelector(".ytp-panel-options")
-	)
-		return;
-
-	const menuHeader = ytOptionsEl.querySelector(".ytp-panel-header");
-	if (menuHeader) menuHeader.appendChild(newOptionsButton);
-
-	ytOptionsObserver.disconnect();
-});
-ytOptionsEl && ytOptionsObserver.observe(ytOptionsEl, { childList: true });
-
 chrome.runtime.onMessage.addListener((message: { action: string }) => {
 	if (message.action === "updateSubtitles") addStyles();
 });

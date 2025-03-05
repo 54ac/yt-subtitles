378a790366f446b3280754cb237c50d02b2bc516
54ac <me@54ac.ovh>
2025-03-05 17:50:52 +0100

    fix eslint config after migration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,19 @@
 import globals from "globals";
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 	tseslint.configs.stylistic,
-	eslintPluginPrettierRecommended,
+	eslintConfigPrettier,
 	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 				...globals.webextensions
 			},
-
 			ecmaVersion: "latest",
 			sourceType: "module"
 		},
@@ -22,7 +21,6 @@ export default tseslint.config(
 		rules: {
 			"no-unused-vars": 1,
 			"no-console": 1,
-
 			"no-unused-expressions": [
 				2,
 				{
